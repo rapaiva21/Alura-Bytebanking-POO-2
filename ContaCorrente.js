a139f9c167d8ea0ef2d25cf1db5dbc560756c222
Carlos Henrique <rapaiva21@gmail.com>
2021-09-19 18:24:10 -0300

    Remoção dos atributos agencia e _Cliente. Agora estão somente no Constructor

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -2,12 +2,9 @@ import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
     static numeroDeContas = 0;
-    agencia;
-    _cliente;
+   
    // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-   _saldo = 0;
-    
-
+   
 
     set cliente(novoValor){
         if(novoValor instanceof Cliente){
@@ -26,6 +23,7 @@ export class ContaCorrente{
     constructor(agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0;
         ContaCorrente.numeroDeContas += 1;
     }
 
